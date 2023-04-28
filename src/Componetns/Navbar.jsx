8a4eb31457ad0804486d05e4f9408bdb1afd7d4c
avikodre03 <akodre111@gmail.com>
2023-04-28 22:59:23 +0530

    ass 13

---
+++ src/Componetns/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../CSS_files/Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <h1>LOGO</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Profile</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
